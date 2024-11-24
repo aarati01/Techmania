f9ec1b1d791b174a3e77a2ae8e0970cf31952b82
aarati01 <131001442+aarati01@users.noreply.github.com>
2024-11-24 01:07:14 -0500

    method to create new product is added

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,6 +3,7 @@ var productModel = require("../models/product");
 var path = require("path");
 
 module.exports = {
+  //  controller  to create new product
   addProduct: async function (req, res) {
     const { pId, pName, pDescription, pCategory, pStock, pPrice, pImage } =
       req.body;
