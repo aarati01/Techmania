9a1787d26feae70fbd723cbcf11a462e07196239
aarati01 <131001442+aarati01@users.noreply.github.com>
2024-11-24 00:53:03 -0500

    New branch name addProuduct-branch is created

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,8 +1,21 @@
-var mongoose = require('mongoose');
-var productModel = require('../models/product');
-var path = require('path');
+var mongoose = require("mongoose");
+var productModel = require("../models/product");
+var path = require("path");
 
 module.exports = {
-
-    
-}
+  addProduct: async function (req, res) {
+    const { pId, pName, pDescription, pCategory, pStock, pPrice, pImage } =
+      req.body;
+    const newProduct = new ProductModel({
+      pId,
+      pName,
+      pDescription,
+      pCategory,
+      pStock,
+      pPrice,
+      pImage,
+    });
+    await newProduct.save();
+    console.log("Product added successfully!");
+  },
+};
